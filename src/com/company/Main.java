3315196f9251dad5fb910bc05acabdf97355854f
gulshoda10 <gulichka_rd@yahoo.com>
2021-06-18 22:01:05 -0700

    first basic structure

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //work on your own branches
+        //create a new class for each feature
+        //and once that push it to a github and pull request
+
+    }
+}
